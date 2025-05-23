a0ee6c0d2c8bcbe422b66b109f86c0f002ee3f79
LeapgenSolution <websites@leapgen.ai>
2025-05-23 16:36:11 -0500

    Post Call Documentation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
-
+import PostCallDocumentation from "./Pages/PostCallDocumentation";
 import Sidebar from "./components/layout/Sidebar";
 import Header from "./components/layout/Header";
 import Dashboard from "./Pages/Dashboard";
 import Appointments from "./Pages/Appointments";
 import Patients from "./Pages/Patients";
+import PatientRecords from "./Pages/PatientRecords";
 import Reports from "./Pages/Reports";
 import Settings from "./Pages/Settings";
 import NotFound from "./Pages/not-found";
@@ -37,9 +38,11 @@ function Router() {
             <Route path="/appointments" component={Appointments} />
             <Route path="/video-call" component={VideoRecorder} />
             <Route path="/patients" component={Patients} />
+            <Route path="/patientrecords" component={PatientRecords}/>
             <Route path="/reports" component={Reports} />
             <Route path="/settings" component={Settings} />
             <Route path="/meeting-room" component={StreamVideoCoreV2} />
+            <Route path="/post-call" component={PostCallDocumentation} />
             <Route component={NotFound} />
           </Switch>
         </main>
