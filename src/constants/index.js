4086f0e0b57657ec2357ae2911390ba660d6fd93
LeapgenSolution <websites@leapgen.ai>
2025-07-01 21:56:27 -0500

    Links changing to https://dev.seismicconnect.com/

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,5 +4,5 @@ export const DOCTOR_PORTAL_URL = "https://calm-smoke-0a7368a0f.6.azurestaticapps
 export const SOS_URL = "https://seismicdockerbackend-test-e0ducsgtggh7ftat.centralus-01.azurewebsites.net/"
 export const STREAM_API_KEY = "72499ykcfb3z"
 // export const REDIRECT_URI = "http://localhost:3000/"
-export const REDIRECT_URI = "https://victorious-mushroom-08b7e7d0f.4.azurestaticapps.net/"
+export const REDIRECT_URI = "https://dev.seismicconnect.com/"
 export const CHATBOT_URL = "https://chat-strlit-gva3h7ekahgdctgk.centralus-01.azurewebsites.net";
