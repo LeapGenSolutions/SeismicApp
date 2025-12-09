182c7cd4e4604daaa9adacb7a06b406ea090a846
Vamshi Krishna <vamshikrishnadulam2504@gmail.com>
2025-12-09 19:14:14 +0530

    updated emotinal empathy and bulk appointment

--- src/components/post-call/EmotionalConnect.jsx
+++ src/components/post-call/EmotionalConnect.jsx
@@ -7,7 +7,7 @@ import LongitudinalSentiment from "./longitudinal_sentiment";
 
 const EmotionalConnect = (props) => {
     const [selectTab, setSelectTab] = useState("Emotional Empathy");
-    const { username, appointmentId, patientId} = props;
+    const { username, appointmentId, appointment} = props;
   return (
     <>
         <Card className="mt-8">
@@ -35,7 +35,7 @@ const EmotionalConnect = (props) => {
                     ))}
                 </div>
                 {selectTab === "Emotional Empathy" && (
-                    <EmotionalEmpathy username={username} appointmentId={appointmentId} patientId={patientId} />
+                    <EmotionalEmpathy username={username} appointmentId={appointmentId} appointment={appointment} />
                 )}
 
                 {selectTab === "Sentiment Analysis" && (
